c0041f85ef1a3337ec1083e108f0c47a81cea7b6
Camiel Luijkx <camiel.luijkx@gmail.com>
2018-03-15 21:37:44 +0100

    logging multiple clients

--- clu.console.demo.net35/Program.cs
+++ clu.console.demo.net35/Program.cs
@@ -16,15 +16,25 @@ namespace clu.console.demo.net35
             public string Message { get; set; }
         }
 
+        private static object Get(Uri webApiUrl)
+        {
+            var client = new WebClient { Headers = { [HttpRequestHeader.ContentType] = "application/json" } };
+
+            var response = client.DownloadString(webApiUrl);
+
+            return JsonConvert.DeserializeObject(response);
+        }
+
         private static object Post(object data, Uri webApiUrl)
         {
-            var client = new WebClient();
-            client.Headers[HttpRequestHeader.ContentType] = "application/json";
+            var client = new WebClient {Headers = {[HttpRequestHeader.ContentType] = "application/json"}};
 
-            var serializerSettings = new JsonSerializerSettings();
-            serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
 
-            string serialisedData = JsonConvert.SerializeObject(data, serializerSettings);
+            var serialisedData = JsonConvert.SerializeObject(data, serializerSettings);
 
             var response = client.UploadString(webApiUrl, serialisedData);
 
@@ -33,31 +43,80 @@ namespace clu.console.demo.net35
 
         private static void TestSomeLogging()
         {
+            //GlobalContext.Properties["correlationId"] = Guid.NewGuid(); // [TODO] 1)
+
             try
             {
-                var data = new PostData { Message = "of course this api works!" };
+                Post(new PostData { Message = "some debug message" }, new Uri("http://localhost/clu.logging.webapi/Logging/Debug"));
+                Post(new PostData { Message = "some error message" }, new Uri("http://localhost/clu.logging.webapi/Logging/Error"));
+                Post(new PostData { Message = "some fatal message" }, new Uri("http://localhost/clu.logging.webapi/Logging/Fatal"));
+                Post(new PostData { Message = "some info message" }, new Uri("http://localhost/clu.logging.webapi/Logging/Info"));
+                Post(new PostData { Message = "some warn message" }, new Uri("http://localhost/clu.logging.webapi/Logging/Warn"));
 
-                var result = Post(data, new Uri("http://localhost/clu.logging.webapi/Logging/Debug"));
+                Post(new PostData { Message = "some secret password" }, new Uri("http://localhost/clu.logging.webapi/Logging/Info"));
+
+                //throw new Exception("some exception occurred");
+
+                //await Log4netLogger.Instance.LogErrorAsync("kaboom!", new ApplicationException("The application exploded!!")); // [TODO] 1)
             }
             catch (Exception ex)
             {
-                throw ex;
+                //await Log4netLogger.Instance.LogErrorAsync("Error trying to do something:", ex); // [TODO] 1)
             }
         }
 
         private static void TestRandomLogging()
         {
+            //GlobalContext.Properties["correlationId"] = Guid.NewGuid(); // [TODO] 1)
+
             try
             {
-                var data = new PostData { Message = "it needs more implementation though..." };
+                var ipsum = Get(new Uri("https://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=0&format=text")); // [TODO] 1)
 
-                var result = Post(data, new Uri("http://localhost/clu.logging.webapi/Logging/Debug"));
+                var random = new Random();
+
+                var dice = random.Next(1, 7);
+
+                switch (dice)
+                {
+                    case 1:
+                    {
+                        Post(new PostData { Message = ipsum.ToString() }, new Uri("http://localhost/clu.logging.webapi/Logging/Debug")); // [TODO] 1)
+                        break;
+                    }
+                    case 2:
+                    {
+                        Post(new PostData { Message = ipsum.ToString() }, new Uri("http://localhost/clu.logging.webapi/Logging/Error")); // [TODO] 1)
+                        break;
+                    }
+                    case 3:
+                    {
+                        Post(new PostData { Message = ipsum.ToString() }, new Uri("http://localhost/clu.logging.webapi/Logging/Fatal")); // [TODO] 1)
+                        break;
+                    }
+                    case 4:
+                    {
+                        Post(new PostData { Message = ipsum.ToString() }, new Uri("http://localhost/clu.logging.webapi/Logging/Info")); // [TODO] 1)
+                        break;
+                    }
+                    case 5:
+                    {
+                        Post(new PostData { Message = ipsum.ToString() }, new Uri("http://localhost/clu.logging.webapi/Logging/Warn")); // [TODO] 1)
+                        break;
+                    }
+                    //case 6:
+                    //{
+                    //    await Log4netLogger.Instance.LogErrorAsync("bad luck", new Exception("no meat today")); // [TODO] 1)
+                    //    break;
+                    //}
+                }
             }
             catch (Exception ex)
             {
-                throw ex;
+                //await Log4netLogger.Instance.LogErrorAsync("Error trying to do something", ex); // [TODO] 1)
             }
         }
+
 
         static void Main(string[] args)
         {
--- clu.console.demo.net461/Program.cs
+++ clu.console.demo.net461/Program.cs
@@ -22,17 +22,17 @@ namespace clu.console.demo.net461
                 await Log4netLogger.Instance.LogErrorAsync("some error message");
                 await Log4netLogger.Instance.LogFatalAsync("some fatal message");
                 await Log4netLogger.Instance.LogInformationAsync("some info message");
-                await Log4netLogger.Instance.LogWarningAsync("some warning message");
+                await Log4netLogger.Instance.LogWarningAsync("some warn message");
 
-                await Log4netLogger.Instance.LogInformationAsync("some stupid password");
+                await Log4netLogger.Instance.LogInformationAsync("some secret password");
 
                 //throw new Exception("some exception occurred");
 
-                await Log4netLogger.Instance.LogErrorAsync("kaboom!", new ApplicationException("The application exploded"));
+                await Log4netLogger.Instance.LogErrorAsync("kaboom!", new ApplicationException("The application exploded!!"));
             }
             catch (Exception ex)
             {
-                await Log4netLogger.Instance.LogErrorAsync("Error trying to do something", ex);
+                await Log4netLogger.Instance.LogErrorAsync("Error trying to do something:", ex);
             }
         }
 
@@ -56,23 +56,35 @@ namespace clu.console.demo.net461
                 switch (dice)
                 {
                     case 1:
+                    {
                         await Log4netLogger.Instance.LogDebugAsync(ipsum);
                         break;
+                    }
                     case 2:
+                    {
                         await Log4netLogger.Instance.LogErrorAsync(ipsum);
                         break;
+                    }
                     case 3:
+                    {
                         await Log4netLogger.Instance.LogFatalAsync(ipsum);
                         break;
+                    }
                     case 4:
+                    {
                         await Log4netLogger.Instance.LogInformationAsync(ipsum);
                         break;
+                    }
                     case 5:
+                    {
                         await Log4netLogger.Instance.LogWarningAsync(ipsum);
                         break;
+                    }
                     case 6:
+                    {
                         await Log4netLogger.Instance.LogErrorAsync("bad luck", new Exception("no meat today"));
                         break;
+                    }
                 }
             }
             catch (Exception ex)
